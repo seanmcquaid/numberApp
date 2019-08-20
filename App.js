0a7a2a425ffa3fbbf9b814e02f9e957cfe4abd07
seanmcquaid <sean.mcquaid2@verizon.net>
2019-08-19 22:42:15 -0400

    added screen component, and worked on layout

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from "./components/Header";
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title="Number App"/>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
